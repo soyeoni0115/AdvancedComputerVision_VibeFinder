6b93729518451eaf70e6bcb0650836f89fb04643
soyeon <happyksy0501@gmail.com>
2026-06-01 15:58:26 +0900

    chore: aborting해결 오류

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from pathlib import Path
+import runpy
 import sys
 
 
 SRC_DIR = Path(__file__).resolve().parent / "src"
+SRC_APP = Path(__file__).resolve().parent / "src" / "app.py"
 sys.path.insert(0, str(SRC_DIR))
 
-import app  # noqa: F401,E402
+runpy.run_path(str(SRC_APP), run_name="__main__")
